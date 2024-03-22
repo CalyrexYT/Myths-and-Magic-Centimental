46d72f2e39b77d245ed97c03d112ccad6d3e5f77
KairokuYT <136025844+CalyrexYT@users.noreply.github.com>
2024-03-22 15:00:00 -0400

    Deleted KillCount.java

--- src/main/java/in/techpro424/skillset_centimental/KillCount.java
+++ src/main/java/in/techpro424/skillset_centimental/KillCount.java
@@ -1,15 +1,1 @@
-package in.techpro424.skillset_centimental;
 
-public class KillCount {
-    private int kills = 0;
-    public KillCount(){
-        setKills(kills);
-    }
-    public int getKills(){
-        return this.kills;
-    }
-
-    public void setKills(int set){
-        this.kills = set;
-    }
-}
