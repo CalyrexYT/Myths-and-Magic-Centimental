d5048dd67954fa0fa4ba38b3e23027771b1c9b67
KairokuYT <136025844+CalyrexYT@users.noreply.github.com>
2024-04-08 09:17:46 -0400

    Added Riptide to Mjolnir

--- src/main/java/in/techpro424/skillset_centimental/item/MjolnirItem.java
+++ src/main/java/in/techpro424/skillset_centimental/item/MjolnirItem.java
@@ -3,6 +3,7 @@ package in.techpro424.skillset_centimental.item;
 import in.techpro424.skillset_centimental.entity.MjolnirEntity;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MovementType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
@@ -10,11 +11,14 @@ import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ToolMaterials;
 import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.UseAction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
 public class MjolnirItem extends AxeItem {
@@ -33,7 +37,7 @@ public class MjolnirItem extends AxeItem {
     public boolean hasRecipeRemainder() {
         return true;
     }
-
+    
     @Override
     public boolean isDamageable() {
         return false;
@@ -47,7 +51,7 @@ public class MjolnirItem extends AxeItem {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
         ItemStack itemStack = user.getStackInHand(hand);
         if (itemStack.getDamage() >= itemStack.getMaxDamage() - 1) return TypedActionResult.fail(itemStack);
-
+        
         user.setCurrentHand(hand);
         return TypedActionResult.consume(itemStack);
     }
@@ -67,22 +71,43 @@ public class MjolnirItem extends AxeItem {
         if (i < 10) {
             return;
         }
+        boolean riptide = playerEntity.isSneaking();
         if (!world.isClient) {
             stack.damage(1, playerEntity, p -> p.sendToolBreakStatus(user.getActiveHand()));
+            
+            if(!riptide) {
+                
+                MjolnirEntity mjolnir = new MjolnirEntity(world, playerEntity, stack);
+                mjolnir.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0f, 2.5f, 1.0f);
 
-            MjolnirEntity mjolnir = new MjolnirEntity(world, playerEntity, stack);
-            mjolnir.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0f, 2.5f, 1.0f);
+                if (playerEntity.getAbilities().creativeMode) mjolnir.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
+                
+                world.spawnEntity(mjolnir);
+                world.playSoundFromEntity(null, mjolnir, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0f, 1.0f);
 
-            if (playerEntity.getAbilities().creativeMode) mjolnir.pickupType = PersistentProjectileEntity.PickupPermission.CREATIVE_ONLY;
-
-            world.spawnEntity(mjolnir);
-            world.playSoundFromEntity(null, mjolnir, SoundEvents.ITEM_TRIDENT_THROW, SoundCategory.PLAYERS, 1.0f, 1.0f);
-
-            if (!playerEntity.getAbilities().creativeMode) playerEntity.getInventory().removeOne(stack);
-
-
+                if (!playerEntity.getAbilities().creativeMode) playerEntity.getInventory().removeOne(stack);
+                
+            }
         }
         playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+        if (riptide) {
+            int j = 5;
+            float f = playerEntity.getYaw();
+            float g = playerEntity.getPitch();
+            float h = -MathHelper.sin(f * ((float)Math.PI / 180)) * MathHelper.cos(g * ((float)Math.PI / 180));
+            float k = -MathHelper.sin(g * ((float)Math.PI / 180));
+            float l = MathHelper.cos(f * ((float)Math.PI / 180)) * MathHelper.cos(g * ((float)Math.PI / 180));
+            float m = MathHelper.sqrt(h * h + k * k + l * l);
+            float n = 3.0f * ((1.0f + (float)j) / 4.0f);
+            playerEntity.addVelocity(h *= n / m, k *= n / m, l *= n / m);
+            playerEntity.useRiptide(20);
+            if (playerEntity.isOnGround()) {
+                float o = 1.1999999f;
+                playerEntity.move(MovementType.SELF, new Vec3d(0.0, 1.1999999284744263, 0.0));
+            }
+            SoundEvent soundEvent = j >= 3 ? SoundEvents.ITEM_TRIDENT_RIPTIDE_3 : (j == 2 ? SoundEvents.ITEM_TRIDENT_RIPTIDE_2 : SoundEvents.ITEM_TRIDENT_RIPTIDE_1);
+            world.playSoundFromEntity(null, playerEntity, soundEvent, SoundCategory.PLAYERS, 1.0f, 1.0f);
+        }
     }
 
     @Override
