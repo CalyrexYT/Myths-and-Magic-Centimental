9e4c29b0c8a0f00b2eb860d8d634744534c904cd
KairokuYT <136025844+CalyrexYT@users.noreply.github.com>
2024-05-20 17:00:16 -0400

    Delete skillset_centimental directory

--- skillset_centimental/world/gen/ModWorldGeneration.java
+++
@@ -1,7 +0,0 @@
-package in.kairoku.skillset_centimental.world.gen;
-
-public class ModWorldGeneration {
-    public static void generateModWorldGen(){
-        ModOreGeneration.generateOres();
-    }
-}
